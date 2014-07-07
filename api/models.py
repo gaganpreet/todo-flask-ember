441cf8556d69df482b1f949bb370487f155e0418
Gaganpreet <gaganpreet.arora@gmail.com>
2014-07-07 09:04:11 +0530

    SQLAlchemy model for Todos

---
+++ api/models.py
@@ -0,0 +1,22 @@
+from database import db
+
+
+class Todo(db.Model):
+    ''' Todo representation '''
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(512))
+
+    is_completed = db.Column(db.Boolean, default=False)
+
+    added_on = db.Column(db.DateTime,
+                         default=db.func.now())
+
+    last_update = db.Column(db.DateTime,
+                            default=db.func.now(),
+                            onupdate=db.func.now())
+
+    def __init__(self, description):
+        self.description = description
+
+    def __repr__(self):
+        return '<{0}>'.format(self.description)
