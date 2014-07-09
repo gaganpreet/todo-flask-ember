06d90e582653caf1fb3cbe4b0228e20ad9c15d4e
Gaganpreet <gaganpreet.arora@gmail.com>
2014-07-09 08:14:34 +0530

    Added heroku db setting

--- run_prod.py
+++ run_prod.py
@@ -6,5 +6,8 @@ from main import create_app
 app = create_app()
 app.config.from_object('config.Production')
 
+if os.environ.get('HEROKU'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
 if __name__ == '__main__':
     app.run()
